55727e63876fea6e23596d2b2d1e5478d7710154
9ta1ls <rpeleh19@gmail.com>
2024-03-09 23:07:24 +0200

    added sass, menu button

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-document.getElementById("gallery-button").addEventListener("click", function() {
-    document.getElementById("photo-gallery").scrollIntoView({ behavior: "smooth" });
+const menuButton = document.getElementById('menu-button');
+const navButtons = document.getElementById('nav-buttons');
+let isClicked = false;
+function displayNone(){
+    navButtons.style.display = 'none';
+}
+menuButton.addEventListener('click', function() {
+    if (isClicked) {
+        navButtons.classList.remove('slide-out');
+        navButtons.classList.add('slide-in');
+        setTimeout(displayNone, 300)
+
+    } else {
+        navButtons.style.display = 'flex';
+        navButtons.classList.remove('slide-in');
+        navButtons.classList.add('slide-out');
+    }
+    isClicked = !isClicked;
 });
